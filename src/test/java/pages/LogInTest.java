efb4514af65a9c497daed7eabce994a91931cba7
andrastoth021 <andrastoth021@gmail.com>
2024-06-25 15:29:25 +0200

    create LogInTest class

---
+++ src/test/java/pages/LogInTest.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LogInTest {
+    private WebDriver webDriver;
+    private LogIn logIn;
+
+    @BeforeEach
+    public void precondition() {
+        webDriver = new FirefoxDriver();
+        webDriver.get("http://localhost:3000/login");
+        logIn = new LogIn(webDriver);
+    }
+
+    @Test
+    public void logInValidCredentials() {
+        logIn.fillUsernameField();
+        logIn.fillPasswordField();
+        logIn.clickOnLogIn();
+
+        String expectedURL = "http://localhost:3000/";
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedURL));
+
+        assertEquals(expectedURL, webDriver.getCurrentUrl());
+    }
+}
