a41a31d017e7581ac956e14e30eff7c0dfdfa88a
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-07-23 14:16:37 +0200

    signinpage and loginpage refactored considering pom

--- src/test/java/pages/LogInTest.java
+++ src/test/java/pages/LogInTest.java
@@ -1,17 +1,24 @@
 package pages;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.authentication.LogIn;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LogInTest {
+    private final Dotenv dotenv = Dotenv.configure().load();
+    private final String username = dotenv.get("REPTILE_USER_USERNAME");
+    private final String password = dotenv.get("REPTILE_USER_PASSWORD");
+    private final String usernameOfQuizMaster = dotenv.get("REPTILE_QUIZMASTER_USERNAME");
+    private final String passwordOfQuizMaster = dotenv.get("REPTILE_QUIZMASTER_PASSWORD");
     private WebDriver webDriver;
     private LogIn logIn;
 
@@ -24,9 +31,9 @@ class LogInTest {
 
     @Test
     public void logInValidCredentials() {
-        logIn.fillUsernameFieldWithUserCredentials();
-        logIn.fillPasswordFieldWithUserCredentials();
-        logIn.clickOnLogIn();
+        logIn.fillFieldById(username, "user-name");
+        logIn.fillFieldById(password, "password");
+        logIn.clickOnButton("LOGIN");
 
         String expectedURL = "http://localhost:3000/";
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
